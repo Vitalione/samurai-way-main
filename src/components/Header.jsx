0285f090ddd05845cf354d2ffbf2858cd2d2cf73
VitalTV <80293501401@mail.ru>
2023-02-07 14:51:57 +0300

    Add module.css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import s from './Header.module.css';
+
 
 const Header = () => {
     return (
-        <header className={"header"}>
+        <header className={s.header}>
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuNtooGgvjkYF-mTL-5UQlARn1jHnQ9YiEbMqujskVQ6UPPb7hrLn9xYhhxpvdyR8qot0&usqp=CAU"/>
         </header>
     );
