7cadbafe91b3f0d1aa185a4e8ca68d6cb27be9c5
VitalTV <80293501401@mail.ru>
2023-02-07 13:14:08 +0300

    Added components

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className={"header"}>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuNtooGgvjkYF-mTL-5UQlARn1jHnQ9YiEbMqujskVQ6UPPb7hrLn9xYhhxpvdyR8qot0&usqp=CAU"/>
+        </header>
+    );
+};
+
+export default Header;
