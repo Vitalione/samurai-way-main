2ebf1ce977d72f716c9c01082431a5c346c874d0
Vitalione <80297741401@mail.ru>
2023-06-27 12:39:03 +0300

    Change Profile.jsx.

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import s from "./Profile.module.css"
 import MyPosts from "./MyPosts/MyPosts";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
+import {updateNewPostText} from "../../redux/state";
 
-const Profile = () => {
+const Profile = (props) => {
+
     return (
         <div>
             <ProfileInfo/>
-            <MyPosts/>
+            <MyPosts posts={props.profilePage.posts}
+                     newPostText={props.profilePage.newPostText}
+                     updateNewPostText={props.updateNewPostText}
+                     addPost={props.addPost}/>
         </div>
     );
 };
