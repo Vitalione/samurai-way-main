556da043a677f507f8f532ae7cadb0f2ef25e8b2
Vitalione <80297741401@mail.ru>
2023-07-14 15:04:32 +0300

    add reducer

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,22 +1,45 @@
 const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY'
 const SEND_MESSAGE = 'SEND-MESSAGE'
 
-  const dialogsReducer = (state, action) => {
-      switch (action.type) {
-          case UPDATE_NEW_MESSAGE_BODY:
-              state.newMessageBody = action.body
-              return state
-          case SEND_MESSAGE:
-              let body = state.newMessageBody
-              state.newMessageBody = ''
-              state.messages.push({id: 7, message: body})
-              return state
-          default:
-              return state
-      }
-  }
+let initialState = {
+    dialogs: [
+        {id: 1, name: 'Vital'},
+        {id: 2, name: 'Evgeny'},
+        {id: 3, name: 'Alex'},
+        {id: 4, name: 'Stepan'},
+        {id: 5, name: 'Katty'},
+        {id: 6, name: 'Helen'}
+    ],
+    messages: [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'How are you'},
+        {id: 3, message: 'Yo'},
+        {id: 4, message: 'Yo'},
+        {id: 5, message: 'Yo'},
+        {id: 6, message: 'Yo'}
+    ],
+    newMessageBody: ''
+}
+const dialogsReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case UPDATE_NEW_MESSAGE_BODY:
+            return {
+                ...state,
+                newMessageBody: action.body
+            };
+        case SEND_MESSAGE:
+            let body = state.newMessageBody
+            return {
+                ...state,
+                newMessageBody: '',
+                messages: [...state.messages, {id: 7, message: body}]
+            };
+        default:
+            return state;
+    }
+}
 
 export const sendMessageActionCreator = () => ({type: SEND_MESSAGE})
 
-export const updateNewMessageBodyActionCreator = (body) => ({type: UPDATE_NEW_MESSAGE_BODY, body:body})
+export const updateNewMessageBodyActionCreator = (body) => ({type: UPDATE_NEW_MESSAGE_BODY, body: body})
 export default dialogsReducer;
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,20 +1,35 @@
 const ADD_POST = 'ADD-POST'
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT'
 
-const profileReducer = (state, action) => {
+let initialState = {
+        posts: [
+            {id: 1, message: "Hi, how are you", likesCount: 0},
+            {id: 1, message: "It's my first post", likesCount: 23}
+        ],
+        newPostText: 'it-kamasutra.com'
+};
+
+const profileReducer = (state = initialState, action) => {
     switch (action.type) {
-        case ADD_POST:
+        case ADD_POST: {
             let newPost = {
                 id: 5,
                 message: state.newPostText,
                 likesCount: 0
-            }
-            state.posts.push(newPost);
-            state.newPostText = ''
-            return state;
-        case UPDATE_NEW_POST_TEXT:
-            state.newPostText = action.newText
-            return state
+            };
+            return  {
+                ...state,
+                posts: [...state.posts, newPost],
+                newPostText: ''
+            };
+        }
+        case UPDATE_NEW_POST_TEXT: {
+            return {
+                ...state,
+                newPostText: action.newText
+
+            };
+        }
         default:
             return state;
     }
